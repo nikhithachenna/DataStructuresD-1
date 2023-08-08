ef33719548f76fae0870f19f2cd3e022250fc344
nikhithachenna <129414325+nikhithachenna@users.noreply.github.com>
2023-08-08 20:13:14 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a;
+	printf("enter the number:\n");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+	printf("the number  %d is even\n",a);
+    }
+    else
+    {
+    printf(" the number %d is odd\n",a);
+    }
+    return 0;
+
+}
